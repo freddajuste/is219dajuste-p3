abd7a447bf5944b99d55078678c3cecb0d5c4b25
freddajuste <fd83@njit.edu>
2018-05-04 11:29:50 -0700

    added functions posterclick and timetext

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -112,7 +112,18 @@ app.controller('HomeController', ['$scope', function($scope) {
 		
 	
 	
-	
+	    $scope.posterClick = function (index) {
+        $scope.movies[index].posterindex++;
+        if ($scope.movies[index].posterindex == $scope.movies[index].posters.length) {
+            $scope.movies[index].posterindex = 0;
+        }
+    }
+
+	$scope.timeText = function (minutes) {
+        $scope.hour = Math.floor(minutes / 60);
+        $scope.min = minutes % 60;
+        return $scope.hour + " hours " + $scope.min + " minutes";
+    }
 	
 	
 	
